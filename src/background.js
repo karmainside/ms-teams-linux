70685e68a64be8bd554502ab05d1cb0ad90fb48c
Kenneth Aasan <k.aasan@sportradar.com>
2017-03-08 17:50:19 +0100

    #10: Fixes prettier config

--- src/background.js
+++ src/background.js
@@ -56,7 +56,7 @@ app.on('ready', function() {
   });
 
   mainWindow.webContents.setUserAgent(
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
   );
 
   mainWindow.loadURL('https://teams.microsoft.com/');
